efee51bb1fb814e7a969d26497796e8042e2a540
2FIVE192 <146088741+2FIVE192@users.noreply.github.com>
2024-12-13 14:13:52 +0700

    Update tsu.c

--- lab3/tsu.c
+++ lab3/tsu.c
@@ -1,20 +1,18 @@
 #include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/printk.h>
-
-#define procfs_name "tsu"
-
-static int __init tsu_init(void)
-{
-    pr_info("Welcome to the Tomsk State University\n");
-    return 0;
-}
-
-static void __exit tsu_exit(void)
-{
-    pr_info("Tomsk State University forever!\n");
-}
-
-MODULE_LICENSE("GPL");
-module_init(tsu_init);
-module_exit(tsu_exit);
+ #include <linux/module.h>
+ #include <linux/printk.h>
+ #include <linux/proc_fs.h>
+ #include <linux/uaccess.h>
+ #include <linux/version.h>
+ #define procfs_name "tsu"
+ static struct proc_dir_entry *our_proc_file = NULL;
+ int init_module(void)
+ {
+ pr_info("Welcome to the Tomsk State University\n");
+ return 0;
+ }
+ void cleanup_module(void)
+ {
+ pr_info("Tomsk State University forever!\n");
+ }
+ MODULE_LICENSE("GPL");
