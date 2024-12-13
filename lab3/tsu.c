f03c40c28f0322d63bc2ee78190e27889d2857c2
2FIVE192 <146088741+2FIVE192@users.noreply.github.com>
2024-12-13 14:14:25 +0700

    Update tsu.c

--- lab3/tsu.c
+++ lab3/tsu.c
@@ -1,13 +1,13 @@
 #include <linux/kernel.h>
- #include <linux/module.h>
- #include <linux/printk.h>
- #include <linux/proc_fs.h>
- #include <linux/uaccess.h>
- #include <linux/version.h>
- #define procfs_name "tsu"
- static struct proc_dir_entry *our_proc_file = NULL;
- int init_module(void)
- {
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/version.h>
+#define procfs_name "tsu"
+static struct proc_dir_entry *our_proc_file = NULL;
+int init_module(void)
+{
  pr_info("Welcome to the Tomsk State University\n");
  return 0;
  }
